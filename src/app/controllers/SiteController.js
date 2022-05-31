e2d80cc7389df620ca20e5803cd8be5d2e32faf3
thanhngodinh <thanh.ngodinh2000@gmail.com>
2022-05-31 23:17:39 +0700

    update CRUD

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,17 +1,16 @@
 const Course = require('../models/Course');
+const { mutipleMongooseToObject } = require('../../util/mongoose');
 
 class SiteController {
     // [GET] /
-    index(req, res) {
-        res.render('home');
-
-        Course.find({}, function (err, course) {
-            if (!err) {
-                res.json(course);
-                return;
-            }
-            res.status(400).json({ error: 'ERROR!' });
-        });
+    index(req, res, next) {
+        Course.find({})
+            .then((courses) => {
+                res.render('home', {
+                    courses: mutipleMongooseToObject(courses),
+                });
+            })
+            .catch(next);
     }
     search(req, res) {
         res.render('search');
--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
 const { engine } = require('express-handlebars');
-const app = express();
-const port = 3000;
-
+const methodOverride = require('method-override');
 const route = require('./routes');
 const db = require('./config/db');
+
+const app = express();
+const port = 3000;
 
 // Connect to DB
 db.connect();
@@ -21,6 +22,8 @@ app.use(
 );
 app.use(express.json());
 
+app.use(methodOverride('_method'));
+
 // HTTP Logger
 // app.use(morgan('combined'));
 
@@ -29,14 +32,17 @@ app.engine(
     'hbs',
     engine({
         extname: '.hbs',
+        helpers: {
+            sum: (a, b) => a + b,
+        },
     }),
 );
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'resources/views'));
+app.set('views', path.join(__dirname, 'resources', 'views'));
 
 // Routes init
 route(app);
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+    console.log(`App listening at port ${port}`);
 });
